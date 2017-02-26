7f8b66fcf786e0c4c422f006a6ddccba56f27cf9
Mrin <mrin9@github>
2017-02-25 20:00:52 -0800

    Updated Angular to Version 4

--- src/main/java/com/app/MainApp.java
+++ src/main/java/com/app/MainApp.java
@@ -22,7 +22,4 @@ SpringBoot Notes
 @Bean      :tells Spring 'here is an instance of this class, please keep hold of it and give it back to me when I ask'.
 @Autowired :says 'please give me an instance of this class, for example, one that I created with an @Bean annotation earlier'.
 
-
-
-
 */
--- src/main/java/com/app/api/MainController.java
+++ src/main/java/com/app/api/MainController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 
-import com.app.identity.*;
-
 @Slf4j
 @ApiIgnore
 @Controller // Dont use RestController as this method is mapping to a static file not a JSON
 public class MainController {
 
-  @RequestMapping(value={"/" })
+  @RequestMapping(value="/")
 	public String index() {
 		return "index.html";
 	}
